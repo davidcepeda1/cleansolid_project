5801884fcdc9515bae5a4e76728d15c626ad628d
davidcepeda1 <davidcepeda751@gmail.com>
2026-06-10 07:48:43 -0500

    refactor: delegar el envio de correos a nueva clase Mailer

--- cleansolid-project/src/solid/01-srp.ts
+++ cleansolid-project/src/solid/01-srp.ts
@@ -5,38 +5,61 @@ interface User {
 
 class UserService {
     loadUser( id: number ) {
-        // Simula la carga de un usuario
         console.log('Cargando usuario con id:', id);
     }
 
     saveUser( user: User ) {
-        // Simula el guardado en base de datos
         console.log('Guardando en base de datos:', user );
+    }
+}
+
+class Mailer {
+    
+    private masterEmail: string = 'fernando@google.com';
+
+    sendEmail( emailList: string[], template: 'to-clients' | 'to-admins' ) {
+        console.log('Enviando correo a los usuarios:', template);
     }
 }
 
 // Esta clase viola el Principio de Responsabilidad Única (SRP)
 class UserBloc {
 
+    private userService: UserService;
+    private mailer: Mailer;
+
+    constructor( userService: UserService, mailer: Mailer ) {
+        this.userService = userService;
+        this.mailer = mailer;
+    }
+
+    loadUser( id: number ) {
+        this.userService.loadUser(id);
+    }
+
+    saveUser( user: User ) {
+        this.userService.saveUser(user);
+    }
+
     notifyUser() {
-        // Simula el envío de notificaciones
-        console.log('Enviando correo a los usuarios');
+        this.mailer.sendEmail(['fernando@google.com'], 'to-clients');
     }
 
 }
 
 class SubscriptionBloc {
     onAddSubscription( subscriptionId: number ) {
-        // Simula la gestión de suscripciones
         console.log('Agregando suscripción:', subscriptionId );
     }
 }
 
 const userService      = new UserService();
-const userBloc         = new UserBloc();
+const mailer           = new Mailer();
+
+const userBloc         = new UserBloc( userService, mailer );
 const subscriptionBloc = new SubscriptionBloc();
 
-userService.loadUser(10);
-userService.saveUser({ id: 10, name: 'Fernando' });
+userBloc.loadUser(10);
+userBloc.saveUser({ id: 10, name: 'Fernando' });
 userBloc.notifyUser();
 subscriptionBloc.onAddSubscription(1234);
